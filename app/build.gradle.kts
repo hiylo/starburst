e7fa8bd56475bfc41d6f5d70c84037b645b38798
hiylo <dev@hiylo.org>
2026-09-18 08:23:23 +0800

    chore(release): 版本号升级至 3.0.0 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.hiylo.starburst"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "2.0.0"
+        versionCode = 8
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
